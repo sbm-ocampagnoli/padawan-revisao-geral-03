c0d5932501cf229d77772ee33eca5e9f5a8079ff
sbm-ocampagnoli <otavio.campagnoli@sbmtechnology.com.br>
2024-08-07 22:41:27 -0300

    add fix name of table

--- backend/backend/sql/001Schema.sql
+++ backend/backend/sql/001Schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Fruit (
+CREATE TABLE fruit (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     quantity INT NOT NULL,
     origin VARCHAR(255) NOT NULL,
@@ -6,7 +6,7 @@ CREATE TABLE Fruit (
 );
 
 
-INSERT INTO Fruit (quantity, origin, importDate) VALUES
+INSERT INTO fruit (quantity, origin, importDate) VALUES
 (100, 'Brazil', '2024-08-01 10:30:00'),
 (200, 'Ecuador', '2024-08-02 14:45:00'),
 (150, 'Colombia', '2024-08-03 09:15:00'),
